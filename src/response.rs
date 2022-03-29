745c0c5dd88a3aa9e2dea0807eb44daf4433e605
Michael Leonhard <michael@leonhardllc.com>
2022-03-29 14:28:48 -0700

    Move Response::reason_phrase to a stand-alone function.

--- src/response.rs
+++ src/response.rs
@@ -223,78 +223,6 @@ impl Response {
     /// # Panics
     /// Panics when called on `Response::Drop` or `Response::GetBodyAndReprocess(..)`.
     #[must_use]
-    pub fn reason_phrase(&self) -> &'static str {
-        // TODO: Move this to a stand-alone funciton, exported in `internal` mod.
-        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-        match self.code() {
-            100 => "Continue",
-            101 => "Switching Protocols",
-            102 => "Processing",
-            103 => "Early Hints",
-            200 => "OK",
-            201 => "Created",
-            202 => "Accepted",
-            203 => "Non-Authoritative Information",
-            204 => "No Content",
-            205 => "Reset Content",
-            206 => "Partial Content",
-            207 => "Multi-Status",
-            208 => "Already Reported",
-            226 => "IM Used",
-            300 => "Multiple Choice",
-            301 => "Moved Permanently",
-            302 => "Found",
-            303 => "See Other",
-            304 => "Not Modified",
-            307 => "Temporary Redirect",
-            308 => "Permanent Redirect",
-            400 => "Bad Request",
-            401 => "Unauthorized",
-            402 => "Payment Required ",
-            403 => "Forbidden",
-            404 => "Not Found",
-            405 => "Method Not Allowed",
-            406 => "Not Acceptable",
-            407 => "Proxy Authentication Required",
-            408 => "Request Timeout",
-            409 => "Conflict",
-            410 => "Gone",
-            411 => "Length Required",
-            412 => "Precondition Failed",
-            413 => "Payload Too Large",
-            414 => "URI Too Long",
-            415 => "Unsupported Media Type",
-            416 => "Range Not Satisfiable",
-            417 => "Expectation Failed",
-            418 => "I'm a teapot",
-            421 => "Misdirected Request",
-            422 => "Unprocessable Entity",
-            423 => "Locked",
-            424 => "Failed Dependency",
-            425 => "Too Early ",
-            426 => "Upgrade Required",
-            428 => "Precondition Required",
-            429 => "Too Many Requests",
-            431 => "Request Header Fields Too Large",
-            451 => "Unavailable For Legal Reasons",
-            500 => "Internal Server Error",
-            501 => "Not Implemented",
-            502 => "Bad Gateway",
-            503 => "Service Unavailable",
-            504 => "Gateway Timeout",
-            505 => "HTTP Version Not Supported",
-            506 => "Variant Also Negotiates",
-            507 => "Insufficient Storage",
-            508 => "Loop Detected",
-            510 => "Not Extended",
-            511 => "Network Authentication Required",
-            _ => "Response",
-        }
-    }
-
-    /// # Panics
-    /// Panics when called on `Response::Drop` or `Response::GetBodyAndReprocess(..)`.
-    #[must_use]
     pub fn content_type(&self) -> &ContentType {
         match self {
             // TODO: Remove these panics.  Restructure somehow.
@@ -364,13 +292,82 @@ impl Debug for Response {
                     f,
                     "Response::Normal({} {}, {:?}, headers={{{}}}, {:?})",
                     self.code(),
-                    self.reason_phrase(),
+                    reason_phrase(self.code()),
                     self.content_type(),
                     headers.join(", "),
                     body
                 )
             }
         }
+    }
+}
+
+#[must_use]
+pub fn reason_phrase(code: u16) -> &'static str {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    match code {
+        100 => "Continue",
+        101 => "Switching Protocols",
+        102 => "Processing",
+        103 => "Early Hints",
+        200 => "OK",
+        201 => "Created",
+        202 => "Accepted",
+        203 => "Non-Authoritative Information",
+        204 => "No Content",
+        205 => "Reset Content",
+        206 => "Partial Content",
+        207 => "Multi-Status",
+        208 => "Already Reported",
+        226 => "IM Used",
+        300 => "Multiple Choice",
+        301 => "Moved Permanently",
+        302 => "Found",
+        303 => "See Other",
+        304 => "Not Modified",
+        307 => "Temporary Redirect",
+        308 => "Permanent Redirect",
+        400 => "Bad Request",
+        401 => "Unauthorized",
+        402 => "Payment Required ",
+        403 => "Forbidden",
+        404 => "Not Found",
+        405 => "Method Not Allowed",
+        406 => "Not Acceptable",
+        407 => "Proxy Authentication Required",
+        408 => "Request Timeout",
+        409 => "Conflict",
+        410 => "Gone",
+        411 => "Length Required",
+        412 => "Precondition Failed",
+        413 => "Payload Too Large",
+        414 => "URI Too Long",
+        415 => "Unsupported Media Type",
+        416 => "Range Not Satisfiable",
+        417 => "Expectation Failed",
+        418 => "I'm a teapot",
+        421 => "Misdirected Request",
+        422 => "Unprocessable Entity",
+        423 => "Locked",
+        424 => "Failed Dependency",
+        425 => "Too Early ",
+        426 => "Upgrade Required",
+        428 => "Precondition Required",
+        429 => "Too Many Requests",
+        431 => "Request Header Fields Too Large",
+        451 => "Unavailable For Legal Reasons",
+        500 => "Internal Server Error",
+        501 => "Not Implemented",
+        502 => "Bad Gateway",
+        503 => "Service Unavailable",
+        504 => "Gateway Timeout",
+        505 => "HTTP Version Not Supported",
+        506 => "Variant Also Negotiates",
+        507 => "Insufficient Storage",
+        508 => "Loop Detected",
+        510 => "Not Extended",
+        511 => "Network Authentication Required",
+        _ => "Response",
     }
 }
 
@@ -396,7 +393,7 @@ pub async fn write_http_response(
     let mut head_bytes: Vec<u8> = format!(
         "HTTP/1.1 {} {}\r\n",
         response.code(),
-        response.reason_phrase()
+        reason_phrase(response.code())
     )
     .into_bytes();
     if response.content_type() != &ContentType::None {
