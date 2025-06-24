a0947d93200b9ffe612bcde9d40b36a7e83a18a3
Michael Leonhard <michael206@gmail.com>
2025-06-24 15:52:52 -0700

    Add load to benchmark.

--- bench/benches/bench.rs
+++ bench/benches/bench.rs
@@ -1,60 +1,166 @@
 //! Run: cargo +nightly bench
 use criterion::{Criterion, SamplingMode, criterion_group, criterion_main};
+use fixed_buffer::{FixedBuf, MalformedInputError};
 use permit::Permit;
+use safe_regex::{Matcher1, regex};
 use safina::executor::Executor;
-use servlin::{HttpServerBuilder, Response, socket_addr_127_0_0_1_any_port};
+use servlin::internal::escape_and_elide;
+use servlin::{HttpServerBuilder, Request, Response, socket_addr_127_0_0_1_any_port};
 use std::io::{ErrorKind, Read, Write};
 use std::net::{Shutdown, SocketAddr};
+use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 
 const WARMUP: Duration = Duration::from_millis(50);
-const MEASUREMENT: Duration = Duration::from_millis(200);
+const MEASUREMENT: Duration = Duration::from_millis(10_000);
 
-fn drop_connection_servlin(c: &mut Criterion) {
+fn deframe_http_response(
+    b: &[u8],
+) -> Result<(usize, Option<core::ops::Range<usize>>), MalformedInputError> {
+    let Some((idx, _)) = b.windows(4).enumerate().find(|(_n, w)| w == b"\r\n\r\n") else {
+        return Ok((0, None));
+    };
+    let head_len = idx + 4;
+    let head = &b[0..head_len];
+    let lowercase_head = String::from_utf8_lossy(head).to_ascii_lowercase();
+    let matcher: Matcher1<_> = regex!(br".*\r\ncontent-length: ([0-9]+)\r\n.*");
+    let Some((content_length_str_bytes,)) = matcher.match_slices(lowercase_head.as_bytes()) else {
+        return Err(MalformedInputError(format!(
+            "response is missing content-length header: {}",
+            escape_and_elide(head, 100),
+        )));
+    };
+    let content_length_str = str::from_utf8(content_length_str_bytes).unwrap();
+    let content_length = usize::from_str(content_length_str).unwrap();
+    let response_length = head_len + content_length;
+    if b.len() < response_length {
+        return Ok((0, None));
+    }
+    Ok((response_length, Some(0..response_length)))
+}
+
+fn start_load_threads(permit: Permit, addr: SocketAddr, num_threads: usize) {
+    for _ in 0..num_threads {
+        let permit_clone = permit.clone();
+        std::thread::Builder::new()
+            .spawn(move || {
+                let mut buf: FixedBuf<65536> = FixedBuf::default();
+                loop {
+                    if permit_clone.is_revoked() {
+                        return;
+                    }
+                    let mut tcp_stream = match std::net::TcpStream::connect_timeout(
+                        &addr,
+                        Duration::from_millis(1000),
+                    ) {
+                        Ok(t) => t,
+                        Err(e) => {
+                            eprintln!("error connecting: {}", e);
+                            std::thread::sleep(Duration::from_millis(10));
+                            continue;
+                        }
+                    };
+                    buf.clear();
+                    for _ in 0..500 {
+                        if permit_clone.is_revoked() {
+                            return;
+                        }
+                        if let Err(e) =
+                            tcp_stream.write_all(b"GET /small_response HTTP/1.1\r\n\r\n")
+                        {
+                            eprintln!("error writing: {}", e);
+                            break;
+                        }
+                        match buf.read_frame(&mut tcp_stream, deframe_http_response) {
+                            Ok(None) => {
+                                eprintln!("error reading: connection closed");
+                                break;
+                            }
+                            Ok(Some(r)) => r,
+                            Err(e) => {
+                                eprintln!("error reading: {}", e);
+                                break;
+                            }
+                        };
+                    }
+                    let _ = tcp_stream.shutdown(Shutdown::Both);
+                }
+            })
+            .unwrap();
+    }
+}
+
+fn do_http_request(addr: SocketAddr, path: &'static str, expected_response: &'static str) {
+    let mut tcp_stream =
+        std::net::TcpStream::connect_timeout(&addr, Duration::from_millis(5000)).unwrap();
+    tcp_stream
+        .set_read_timeout(Some(Duration::from_millis(5000)))
+        .unwrap();
+    tcp_stream
+        .write_all(format!("GET {path} HTTP/1.1\r\n\r\n").as_bytes())
+        .unwrap();
+    tcp_stream.shutdown(Shutdown::Write).unwrap();
+    let mut body = String::new();
+    tcp_stream.read_to_string(&mut body).unwrap();
+    assert_eq!(&body, expected_response);
+    let _ = tcp_stream.shutdown(Shutdown::Both);
+}
+
+fn measure_servlin(c: &mut Criterion) {
     let permit = Permit::new();
     safina::timer::start_timer_thread();
-    let handler = |_req| Response::drop_connection();
+    let handler = |req: Request| match req.url.path() {
+        "/drop_connection" => Response::drop_connection(),
+        "/small_response" => Response::text(200, "small_response1"),
+        "/slow" => {
+            std::thread::sleep(Duration::from_secs(1));
+            Response::ok_200()
+        }
+        _ => Response::not_found_404(),
+    };
     let executor: Arc<Executor> = Executor::new(4, 4).unwrap();
     let (addr, _stopped_receiver) = executor
         .block_on(
             HttpServerBuilder::new()
                 .listen_addr(socket_addr_127_0_0_1_any_port())
-                .max_conns(1000)
+                .max_conns(10000)
                 .small_body_len(64 * 1024)
                 .permit(permit.new_sub())
                 .spawn(handler),
         )
         .unwrap();
-    let mut group = c.benchmark_group("group");
+    start_load_threads(permit.new_sub(), addr, 2000);
+    let mut group = c.benchmark_group("measure_servlin");
     group
         .sampling_mode(SamplingMode::Flat)
         .warm_up_time(WARMUP)
         .measurement_time(MEASUREMENT);
-    group.bench_function("drop_connection_servlin", move |b| {
+    group.bench_function("small_response", move |b| {
+        b.iter(move || do_http_request(addr, "/small_response", "HTTP/1.1 200 OK\r\ncontent-type: text/plain; charset=UTF-8\r\ncontent-length: 15\r\n\r\nsmall_response1"));
+    });
+    group.bench_function("drop_connection", move |b| {
+        b.iter(move || do_http_request(addr, "/drop_connection", ""));
+    });
+    group.bench_function("timeout", move |b| {
         b.iter(move || {
             let mut tcp_stream =
                 std::net::TcpStream::connect_timeout(&addr, Duration::from_millis(5000)).unwrap();
-            tcp_stream.write_all(b"M / HTTP/1.1\r\n\r\n").unwrap();
-            tcp_stream.shutdown(Shutdown::Write).unwrap();
-            assert_eq!(0, tcp_stream.read(&mut [0u8; 1]).unwrap());
+            tcp_stream.write_all(b"GET /slow HTTP/1.1\r\n\r\n").unwrap();
+            tcp_stream
+                .set_read_timeout(Some(Duration::from_millis(10)))
+                .unwrap();
+            let kind = tcp_stream
+                .read_to_string(&mut String::new())
+                .unwrap_err()
+                .kind();
+            assert!(
+                kind == ErrorKind::TimedOut || kind == ErrorKind::WouldBlock,
+                "kind={kind:?}"
+            );
         });
     });
     group.finish();
-}
-
-fn get_body(addr: SocketAddr) {
-    let mut tcp_stream =
-        std::net::TcpStream::connect_timeout(&addr, Duration::from_millis(5000)).unwrap();
-    tcp_stream.write_all(b"GET / HTTP/1.1\r\n\r\n").unwrap();
-    tcp_stream.shutdown(Shutdown::Write).unwrap();
-    let mut body = String::new();
-    tcp_stream.read_to_string(&mut body).unwrap();
-    assert_eq!(
-        "HTTP/1.1 200 OK\r\ncontent-type: text/plain; charset=UTF-8\r\ncontent-length: 5\r\n\r\nbody1",
-        &body
-    );
-    let _ = tcp_stream.shutdown(Shutdown::Both);
 }
 
 // Crashes with:
@@ -96,79 +202,9 @@ fn get_body(addr: SocketAddr) {
 //     group.finish();
 // }
 
-fn small_servlin(c: &mut Criterion) {
-    let permit = Permit::new();
-    safina::timer::start_timer_thread();
-    let handler = |_req| Response::text(200, "body1");
-    let executor: Arc<Executor> = Executor::new(4, 4).unwrap();
-    let (addr, _stopped_receiver) = executor
-        .block_on(
-            HttpServerBuilder::new()
-                .listen_addr(socket_addr_127_0_0_1_any_port())
-                .max_conns(1000)
-                .small_body_len(64 * 1024)
-                .permit(permit.new_sub())
-                .spawn(handler),
-        )
-        .unwrap();
-    let mut group = c.benchmark_group("group");
-    group
-        .sampling_mode(SamplingMode::Flat)
-        .warm_up_time(WARMUP)
-        .measurement_time(MEASUREMENT);
-    group.bench_function("small_servlin", move |b| b.iter(move || get_body(addr)));
-    group.finish();
-}
-
-fn client_timeout_servlin(c: &mut Criterion) {
-    let permit = Permit::new();
-    safina::timer::start_timer_thread();
-    let handler = |_req| {
-        std::thread::sleep(Duration::from_secs(1));
-        Response::text(200, "body1")
-    };
-    let executor: Arc<Executor> = Executor::new(4, 4).unwrap();
-    let (addr, _stopped_receiver) = executor
-        .block_on(
-            HttpServerBuilder::new()
-                .listen_addr(socket_addr_127_0_0_1_any_port())
-                .max_conns(1000)
-                .small_body_len(64 * 1024)
-                .permit(permit.new_sub())
-                .spawn(handler),
-        )
-        .unwrap();
-    let mut group = c.benchmark_group("group");
-    group
-        .sampling_mode(SamplingMode::Flat)
-        .warm_up_time(WARMUP)
-        .measurement_time(MEASUREMENT);
-    group.bench_function("client_timeout_servlin", move |b| {
-        b.iter(move || {
-            let mut tcp_stream =
-                std::net::TcpStream::connect_timeout(&addr, Duration::from_millis(5000)).unwrap();
-            tcp_stream.write_all(b"GET / HTTP/1.1\r\n\r\n").unwrap();
-            tcp_stream
-                .set_read_timeout(Some(Duration::from_millis(10)))
-                .unwrap();
-            let kind = tcp_stream
-                .read_to_string(&mut String::new())
-                .unwrap_err()
-                .kind();
-            assert!(
-                kind == ErrorKind::TimedOut || kind == ErrorKind::WouldBlock,
-                "kind={kind:?}"
-            );
-        });
-    });
-    group.finish();
-}
-
 criterion_group!(
     benches,
-    client_timeout_servlin,
-    drop_connection_servlin,
-    small_servlin,
+    measure_servlin,
     // small_tokio,
 );
 criterion_main!(benches);
